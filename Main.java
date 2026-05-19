99bd25dab510df8c3b86819f594c24e5f7271643
Ariel Alvarenga <124457834+shzest@users.noreply.github.com>
2026-05-19 03:12:32 -0300

    Update vehicle initialization and registration methods

--- Main.java
+++ Main.java
@@ -2,12 +2,16 @@ public class Main {
     public static void main(String[] args) {
         CentroControl centro = new CentroControl();
 
-        Vehiculo dron = new DronTransporte("Dron1");
-        Vehiculo camion = new CamionAutonomo("Camion1");
+        Vehiculo dron = new DronTransporte("DRON-001","DRON","KZN","20");
+        Vehiculo camion = new CamionAutonomo("CAM-001","CAMION","CANIA","100");
 
-        centro.registrarUnidad(dron);
-        centro.registrarUnidad(camion);
-
+        centro.registrarVehiculo(dron);
+        centro.registrarVehiculo(camion);
+        centro.listarVehiculos();
+        centro.buscarVehiculo("DRON-001");
         centro.monitorearFlota();
+        centro.modificarDatosVehiculo("CAM-001","CAMION","SCANIA","110");
+        centro.listarVehiculos();
+        centro.mostrarConectables();
     }
 }
