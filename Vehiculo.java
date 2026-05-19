062fc7dae5a91e3a56b667a7d39412f42a5b1003
Ariel Alvarenga <124457834+shzest@users.noreply.github.com>
2026-05-19 03:11:47 -0300

    Refactor Vehiculo constructor and add new properties

--- Vehiculo.java
+++ Vehiculo.java
@@ -1,13 +1,26 @@
 public abstract class Vehiculo {
     private final String id;
+    private String tipoVehiculo;
+    private String velMax;
+    private String modelo;
 
-    public Vehiculo(String id) {
+    public Vehiculo( String id, String tipoVehiculo, String modelo, String velMax) {
         this.id=id;
+        this.velMax =velMax;
+        this.tipoVehiculo=tipoVehiculo;
+        this.modelo=modelo;
     }
 
-    public String getId() {
-        return id;
+    public String getId(){ return id; }
+    public String getVelMax() {
+        return velMax;
     }
+    public String getTipoVehiculo(){ return tipoVehiculo;}
+    public String getModelo() {return modelo; }
+
+    public void setVelMax(String velMax) {this.velMax = velMax; }
+    public void setTipoVehiculo( String tipoVehiculo) {this.tipoVehiculo=tipoVehiculo; }
+    public void setModelo (String modelo ) {this.modelo=modelo;}
 
     public abstract void patronMovimiento();
 }
